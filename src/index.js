0538a06b325ade12ecaad7da52e609c4ef5a1c66
TheJSGirl <prinegi100@gmail.com>
2019-04-11 20:38:17 +0530

    listening server & emit event

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:9000');
+
+socket.on('connect', async() => {
+    if(window.location.hash) {
+        const hashID = window.location.hash.substring(1);
+         socket.emit('hello', { hashID });
+    }
+});
